74fe239ef0e0072af080b45ee7d378d3836dbd60
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-09-28 12:38:07 +0300

    add placeholder state flow

--- app/src/main/java/com/ferdinand/pdftestapp/main/PdfListFragment.kt
+++ app/src/main/java/com/ferdinand/pdftestapp/main/PdfListFragment.kt
@@ -8,22 +8,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.ferdinand.pdftestapp.ui.theme.PdfTestAppTheme
+import com.ferdinand.pdftestapp.viewmodel.PdfViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PdfListFragment : Fragment() {
 
+    private val viewModel: PdfViewModel by viewModels()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
             setContent {
+                val text by viewModel.placeholder.collectAsState()
                 PdfTestAppTheme {
                     // A surface container using the 'background' color from the theme
                     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                        Text("Hello Android")
+                        Text(text = text)
                     }
                 }
             }
--- app/src/main/java/com/ferdinand/pdftestapp/viewmodel/PdfViewModel.kt
+++ app/src/main/java/com/ferdinand/pdftestapp/viewmodel/PdfViewModel.kt
@@ -2,8 +2,12 @@ package com.ferdinand.pdftestapp.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class PdfViewModel @Inject constructor() : ViewModel() {
+
+    val placeholder = MutableStateFlow("Hello Android").asStateFlow()
 }
