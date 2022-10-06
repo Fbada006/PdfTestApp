f4d508855e0882cf7be5f0da81a19a6d0333213e
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-10-06 22:47:34 +0300

    add db tests

---
+++ app/src/test/java/com/ferdinand/pdftestapp/utils/FakeFileGenerator.kt
@@ -0,0 +1,8 @@
+package com.ferdinand.pdftestapp.utils
+
+import com.ferdinand.pdftestapp.data.models.DbFavoritePdfFile
+
+val dbFavFiles = listOf(
+    DbFavoritePdfFile(2),
+    DbFavoritePdfFile(3)
+)
