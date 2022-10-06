9141da604bb331325c45b04466802737322ffa35
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-10-06 22:01:08 +0300

    improve colours and UI with red color and pdf image

--- app/src/main/java/com/ferdinand/pdftestapp/ui/composables/PdfItem.kt
+++ app/src/main/java/com/ferdinand/pdftestapp/ui/composables/PdfItem.kt
@@ -3,12 +3,16 @@ package com.ferdinand.pdftestapp.ui.composables
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PictureAsPdf
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import com.ferdinand.pdftestapp.R
 import com.ferdinand.pdftestapp.models.PdfFile
@@ -27,10 +31,18 @@ fun PdfItem(
             modifier = Modifier.padding(dimensionResource(id = R.dimen.size_8)),
             verticalAlignment = Alignment.CenterVertically
         ) {
+
+            Icon(
+                imageVector = Icons.Filled.PictureAsPdf,
+                tint = MaterialTheme.colors.secondary,
+                contentDescription = stringResource(id = R.string.cd_pdf_icon),
+                modifier = Modifier.padding(end = dimensionResource(id = R.dimen.size_8))
+            )
+
             Text(
                 text = pdfFile.pdfName,
                 style = MaterialTheme.typography.h6,
-                maxLines = 1,
+                maxLines = 2,
                 overflow = TextOverflow.Ellipsis,
                 modifier = Modifier
                     .weight(1f)
