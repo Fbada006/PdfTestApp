322426b835d9a2e7617503a6c902c48aa6839958
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-10-07 10:57:55 +0300

    add more tests to viewmodel

--- app/src/test/java/com/ferdinand/pdftestapp/viewmodel/PdfViewModelTest.kt
+++ app/src/test/java/com/ferdinand/pdftestapp/viewmodel/PdfViewModelTest.kt
@@ -1,12 +1,8 @@
 package com.ferdinand.pdftestapp.viewmodel
 
-import android.content.Context
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import app.cash.turbine.test
-import com.ferdinand.pdftestapp.data.PdfDao
-import com.ferdinand.pdftestapp.data.PdfDatabase
+import app.cash.turbine.testIn
 import com.ferdinand.pdftestapp.mappers.toPresentationModel
 import com.ferdinand.pdftestapp.models.PdfEvent
 import com.ferdinand.pdftestapp.models.state.PdfQueryState
@@ -19,32 +15,17 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
-import java.io.IOException
 
 @Config(sdk = [32])
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class PdfViewModelTest {
 
-    private lateinit var db: PdfDatabase
-    private lateinit var pdfDao: PdfDao
-    private val context = ApplicationProvider.getApplicationContext<Context>()
-
     private val pdfRepo = mockk<PdfRepo>(relaxed = true)
     private val pdfViewModel = PdfViewModel(pdfRepo)
-
-    @Before
-    fun createDb() {
-        db = Room.inMemoryDatabaseBuilder(
-            context, PdfDatabase::class.java
-        ).build()
-        pdfDao = db.pdfDao()
-    }
 
     @Test
     fun `get all pdf files returns valid list`() = runTest {
@@ -93,13 +74,94 @@ class PdfViewModelTest {
     }
 
     @Test
-    fun `adding file to fav works properly with a non fav pdf`() {
+    fun `get search pdf files returns valid list`() = runTest {
+        // Given
+        coEvery { pdfRepo.getPdfListBasedOnQuery(any()) } returns Resource.Success(pdfFiles)
 
+        // When
+        pdfViewModel.handleEvent(PdfEvent.SearchEvent)
+
+        // Assert flow has correct data
+        pdfViewModel.filteredPdfState.test {
+            assertThat(awaitItem()).isEqualTo(PdfQueryState(isLoading = false, listData = pdfFiles.map {
+                it.toPresentationModel()
+            }))
+        }
     }
 
-    @After
-    @Throws(IOException::class)
-    fun closeDb() {
-        db.close()
+    @Test
+    fun `get search pdf files returns empty list exception`() = runTest {
+        val exception = EmptyListException()
+        // Given
+        coEvery { pdfRepo.getPdfListBasedOnQuery(any()) } returns Resource.Error(exception)
+
+        // When
+        pdfViewModel.handleEvent(PdfEvent.SearchEvent)
+
+        // Assert flow has correct data
+        pdfViewModel.filteredPdfState.test {
+            assertThat(awaitItem()).isEqualTo(PdfQueryState(isLoading = false, error = exception))
+        }
+    }
+
+    @Test
+    fun `get search pdf files returns general exception`() = runTest {
+        val exception = NullPointerException()
+        // Given
+        coEvery { pdfRepo.getPdfListBasedOnQuery(any()) } returns Resource.Error(exception)
+
+        // When
+        pdfViewModel.handleEvent(PdfEvent.SearchEvent)
+
+        // Assert flow has correct data
+        pdfViewModel.filteredPdfState.test {
+            assertThat(awaitItem()).isEqualTo(PdfQueryState(isLoading = false, error = exception))
+        }
+    }
+
+    @Test
+    fun `get file based on id returns valid file`() = runTest {
+        // Given
+        coEvery { pdfRepo.getPdfFileBasedOnId(pdfFiles.first().id) } returns Resource.Success(pdfFiles.first())
+
+        // When
+        pdfViewModel.handleEvent(PdfEvent.DisplayFileDetailsEvent(pdfFiles.first().id))
+
+        // Assert flow has correct data
+        pdfViewModel.singlePdfState.test {
+            assertThat(awaitItem()).isEqualTo(PdfQueryState(isLoading = false, singlePdfData = pdfFiles.first().toPresentationModel()))
+        }
+    }
+
+    @Test
+    fun `get file based on id returns general exception`() = runTest {
+        val exception = NullPointerException()
+        // Given
+        coEvery { pdfRepo.getPdfFileBasedOnId(pdfFiles.first().id) } returns Resource.Error(exception)
+
+        // When
+        pdfViewModel.handleEvent(PdfEvent.DisplayFileDetailsEvent(pdfFiles.first().id))
+
+        // Assert flow has correct data
+        pdfViewModel.singlePdfState.test {
+            assertThat(awaitItem()).isEqualTo(PdfQueryState(isLoading = false, error = exception))
+        }
+    }
+
+    @Test
+    fun `dismissing error sets error flows to null correctly`() = runTest {
+        pdfViewModel.handleEvent(PdfEvent.ErrorDismissedEvent)
+
+        val singleTurbine = pdfViewModel.singlePdfState.testIn(this)
+        val filteredTurbine = pdfViewModel.filteredPdfState.testIn(this)
+        val allTurbine = pdfViewModel.pdfQueryState.testIn(this)
+
+        assertThat(singleTurbine.awaitItem().error).isNull()
+        assertThat(filteredTurbine.awaitItem().error).isNull()
+        assertThat(allTurbine.awaitItem().error).isNull()
+
+        singleTurbine.cancel()
+        filteredTurbine.cancel()
+        allTurbine.cancel()
     }
 }
