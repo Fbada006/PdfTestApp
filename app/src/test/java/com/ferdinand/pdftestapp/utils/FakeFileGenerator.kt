a8bb40683695dbac844912a9ebc4e674c17961bf
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-10-07 09:51:32 +0300

    add more tests including placeholders

--- app/src/test/java/com/ferdinand/pdftestapp/utils/FakeFileGenerator.kt
+++ app/src/test/java/com/ferdinand/pdftestapp/utils/FakeFileGenerator.kt
@@ -1,8 +1,18 @@
 package com.ferdinand.pdftestapp.utils
 
+import android.net.Uri
 import com.ferdinand.pdftestapp.data.models.DbFavoritePdfFile
+import com.ferdinand.pdftestapp.models.PdfFile
 
 val dbFavFiles = listOf(
     DbFavoritePdfFile(2),
-    DbFavoritePdfFile(3)
+    DbFavoritePdfFile(3),
+    DbFavoritePdfFile(4)
+)
+
+val pdfFiles = listOf(
+    PdfFile(id = 0, pdfName = "Name 0", uri = Uri.parse(""), isFavourite = false),
+    PdfFile(id = 1, pdfName = "Name 1", uri = Uri.parse(""), isFavourite = false),
+    PdfFile(id = 2, pdfName = "Name 2", uri = Uri.parse(""), isFavourite = false),
+    PdfFile(id = 3, pdfName = "Name 3", uri = Uri.parse(""), isFavourite = false)
 )
---
+++ app/src/test/java/com/ferdinand/pdftestapp/viewmodel/PdfViewModelTest.kt
@@ -0,0 +1,102 @@
+package com.ferdinand.pdftestapp.viewmodel
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import app.cash.turbine.test
+import com.ferdinand.pdftestapp.data.PdfDao
+import com.ferdinand.pdftestapp.data.PdfDatabase
+import com.ferdinand.pdftestapp.models.PdfEvent
+import com.ferdinand.pdftestapp.models.state.PdfQueryState
+import com.ferdinand.pdftestapp.repo.PdfRepo
+import com.ferdinand.pdftestapp.utils.EmptyListException
+import com.ferdinand.pdftestapp.utils.Resource
+import com.ferdinand.pdftestapp.utils.pdfFiles
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import java.io.IOException
+
+@Config(sdk = [32])
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class PdfViewModelTest {
+
+    private lateinit var db: PdfDatabase
+    private lateinit var pdfDao: PdfDao
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    private val pdfRepo = mockk<PdfRepo>(relaxed = true)
+    private val pdfViewModel = PdfViewModel(pdfRepo)
+
+    @Before
+    fun createDb() {
+        db = Room.inMemoryDatabaseBuilder(
+            context, PdfDatabase::class.java
+        ).build()
+        pdfDao = db.pdfDao()
+    }
+
+    @Test
+    fun `get all pdf files returns valid list`() = runTest {
+        // Given
+        coEvery { pdfRepo.getPdfList() } returns Resource.Success(pdfFiles)
+
+        // When
+        pdfViewModel.handleEvent(PdfEvent.GetAllFilesEvent)
+
+        // Assert flow has correct data
+        pdfViewModel.pdfQueryState.test {
+            assertThat(awaitItem()).isEqualTo(PdfQueryState(isLoading = false, listData = pdfFiles))
+        }
+    }
+
+    @Test
+    fun `get all pdf files returns empty list exception`() = runTest {
+        val exception = EmptyListException()
+        // Given
+        coEvery { pdfRepo.getPdfList() } returns Resource.Error(exception)
+
+        // When
+        pdfViewModel.handleEvent(PdfEvent.GetAllFilesEvent)
+
+        // Assert flow has correct data
+        pdfViewModel.pdfQueryState.test {
+            assertThat(awaitItem()).isEqualTo(PdfQueryState(isLoading = false, error = exception))
+        }
+    }
+
+    @Test
+    fun `get all pdf files returns general exception`() = runTest {
+        val exception = NullPointerException()
+        // Given
+        coEvery { pdfRepo.getPdfList() } returns Resource.Error(exception)
+
+        // When
+        pdfViewModel.handleEvent(PdfEvent.GetAllFilesEvent)
+
+        // Assert flow has correct data
+        pdfViewModel.pdfQueryState.test {
+            assertThat(awaitItem()).isEqualTo(PdfQueryState(isLoading = false, error = exception))
+        }
+    }
+
+    @Test
+    fun `adding file to fav works properly with a non fav pdf`() {
+
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+}
